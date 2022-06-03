bc487dda0362061225d8bd4fa539681846b81df6
vinicius <vinisurf1206@gmail.com>
2022-06-02 22:33:03 -0300

    tarefa concluida

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int contador = 0;
+Console.Write("Quantas vezes?  ");
+int qtdvezes = Convert.ToInt32(Console.ReadLine());
+
+while (contador < qtdvezes)
+
+{
+
+    contador = contador + 1;
+    Console.WriteLine($"{contador}x Santos maior time.");
+
+}
